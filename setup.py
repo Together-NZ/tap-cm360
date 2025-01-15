bbdac2b54eada56b36df6d4ad3681934c0d93552
Peter Wang <pw33537515@gmail.com>
2025-01-16 02:16:06 +1300

    simplified setup.py

--- setup.py
+++ setup.py
@@ -3,23 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="tap-cm360",
     version="0.0.1",
-    description="Singer tap for CM360, built with the Meltano Singer SDK.",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    author="Peter Wang",
-    author_email="peter@wearetogether.co.nz",
-    url="https://github.com/Together-NZ/tap-cm360",  # Update with your repository URL
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-    ],
-    packages=find_packages(include=["tap_cm360", "tap_cm360.*"]),
-    python_requires=">=3.9",
+    packages=find_packages(),
     install_requires=[
         "singer-sdk",
         "requests",
@@ -27,14 +11,5 @@ setup(
         "google-api-python-client",
         "httplib2",
     ],
-    extras_require={
-        "s3": ["fs-s3fs~=1.1.1"],
-    },
-    entry_points={
-        "console_scripts": [
-            "tap-cm360=tap_cm360.tap:Tapcm360.cli",
-        ],
-    },
     include_package_data=True,
-    license="Apache-2.0",
 )
